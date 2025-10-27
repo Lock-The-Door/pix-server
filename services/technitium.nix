1debdb007f4ac8f36bfca1a38fbbb921c9a86791
Lock-The-Door <Lock-The-Door@outlook.com>
2025-10-26 22:44:04 -0400

    Update technitium.nix

--- services/technitium.nix
+++ services/technitium.nix
@@ -1,14 +1,15 @@
 { ... }:
 {
-  services.technitium-dns-server = {
-    enable = true;
-    openFirewall = true;
-    firewallTCPPorts = [ 53 443 853 53443 ];
-  };
   fileSystems."/var/lib/technitium-dns-server" = {
       depends = [ "/data" ];
       device = "/data/technitium";
       fsType = "none";
       options = [ "bind" ];
+  };
+
+  services.technitium-dns-server = {
+    enable = true;
+    openFirewall = true;
+    firewallTCPPorts = [ 53 443 853 53443 ];
   };
 }
