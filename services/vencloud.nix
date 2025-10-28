fc868352b7480da7c1d7419df97147bf078d28e2
Lock-The-Door <Lock-The-Door@outlook.com>
2025-10-28 04:44:31 -0400

    Update vencloud.nix

--- services/vencloud.nix
+++ services/vencloud.nix
@@ -27,13 +27,12 @@
       networking.firewall.allowedTCPPorts = [ 8080 ];
       services.vencloud = {
         enable = true;
-        redisService = "redis-vencloud.service";
         allowedUsers = [ 374284798820352000 ];
         proxyHeader = "X-Forwarded-For";
         environmentFiles = [ "/run/secrets/vencloud.env" ];
       };
 
-      services.redis.servers."vencloud" = {
+      services.redis.servers."" = {
         enable = true;
         save = [ [ 300 1 ] [ 60 10 ] ];
       };
