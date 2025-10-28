b1a9ced2e44d87c353b4b02ed0e90adbede23e8b
Lock-The-Door <Lock-The-Door@outlook.com>
2025-10-28 06:43:33 -0400

    change disko device

--- disko-config.nix
+++ disko-config.nix
@@ -3,7 +3,7 @@
     disk = {
       primary = {
         type = "disk";
-        device = "/dev/vda";
+        device = "/dev/mmcblk0";
         content = {
           type = "gpt";
           partitions = {
