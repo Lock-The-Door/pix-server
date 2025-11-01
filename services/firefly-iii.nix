eccd0b7826d1d524c9b8b0cb34ab06b35f8fec78
Lock-The-Door <Lock-The-Door@outlook.com>
2025-11-01 14:07:43 -0400

    Revert "maybe maybe caddy"
    This reverts commit 61d9ab6621fa0c4fdba69afb8304276cd6c4ebf7.

--- services/firefly-iii.nix
+++ services/firefly-iii.nix
@@ -18,22 +18,24 @@ in { pkgs, ... }: {
     config = { ... }: {
       networking.firewall.allowedTCPPorts = [ 80 ];
 
-      # services.caddy = {
-      #   enable = true;
-      #   group = "firefly-iii";
-      #   globalConfig = ''
-      #     servers {
-      #     	trusted_proxies static private_ranges
-      #     }
-      #   '';
-      #   extraConfig = ''
-      #     :80 {
-      #      	root * ${pkgs.firefly-iii}/public
-      #       php_fastcgi unix//run/phpfpm/firefly-iii.sock
-      #       file_server
-      #     }
-      #   '';
-      # };
+      services.caddy = {
+        enable = true;
+        group = "firefly-iii";
+        globalConfig = ''
+          servers {
+          	trusted_proxies static private_ranges
+          }
+        '';
+        extraConfig = ''
+          :80 {
+           	encode
+            php_fastcgi unix//run/phpfpm/firefly-iii.sock
+            file_server {
+            	root ${pkgs.firefly-iii}/public
+            }
+          }
+        '';
+      };
 
       services.firefly-iii = {
         enable = true;
