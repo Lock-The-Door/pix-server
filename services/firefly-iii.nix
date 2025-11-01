607bd99552c0b4aeae12634461995a52d13cf24c
Lock-The-Door <Lock-The-Door@outlook.com>
2025-11-01 18:48:39 -0400

    omit cookie domain

--- services/firefly-iii.nix
+++ services/firefly-iii.nix
@@ -3,9 +3,6 @@ in { pkgs, ... }: {
   services.caddy.virtualHosts."pix.pug-squeaker.ts.net:8024" = {
     extraConfig = "reverse_proxy 192.168.103.100:80";
   };
-  services.caddy.virtualHosts."pix.pug-squeaker.ts.net:8025" = {
-    extraConfig = "reverse_proxy 192.168.103.100:8080";
-  };
   networking.firewall.allowedTCPPorts = [ 8024 ];
 
   containers.firefly-iii = {
@@ -47,7 +44,6 @@ in { pkgs, ... }: {
           APP_URL = fireflyUrl;
           APP_KEY_FILE = "/run/secrets/firefly-iii";
           TZ = "America/Toronto";
-          COOKIE_DOMAIN = "https://pix.pug-squeaker.ts.net:8024";
           COOKIE_SECURE = "true";
           ENABLE_EXTERNAL_MAP = "true";
           ENABLE_EXCHANGE_RATES = "true";
