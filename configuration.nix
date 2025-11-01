2b0c830bd03ea0dd57ee4879da83fb3d69e9ed78
Lock-The-Door <Lock-The-Door@outlook.com>
2025-11-01 00:44:51 -0400

    install nmap

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,7 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [ vim croc git disko ];
+  environment.systemPackages = with pkgs; [ vim croc git disko  nmap];
 
   # Nix configuration
   nix.settings = {
