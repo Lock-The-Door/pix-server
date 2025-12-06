9fbcca12c5b43b16fbca6baef9644d6520ac22c4
Lock-The-Door <Lock-The-Door@outlook.com>
2025-12-05 23:57:01 -0500

    Revert "use nixos-init"
    This reverts commit a01f138eec66f36a593681c68486373b76ba31e1.

--- configuration.nix
+++ configuration.nix
@@ -30,8 +30,6 @@
   boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.grub.configurationLimit = 2;
 
-  system.nixos-init.enable = true;
-
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
