42f4b9906cd71e6bba68445749c85bd379bb18a0
Lock-The-Door <Lock-The-Door@outlook.com>
2025-12-05 21:17:49 -0500

    croc relay w/o pass

--- services/croc.nix
+++ services/croc.nix
@@ -1,7 +1,11 @@
-{...}: {
+{ lib, ... }: {
 	services.croc = {
 		enable = true;
 		pass = "";
 		openFirewall = true;
 	};
+
+	systemd.services.croc.serviceConfig.ExecStart = "${pkgs.croc}/bin/croc ${lib.optionalString (services.croc.pass != "") "--pass '${services.croc.pass}'"} ${lib.optionalString services.croc.debug "--debug"} relay --ports ${
+          lib.concatMapStringsSep "," toString services.croc.ports
+        }";
 }
